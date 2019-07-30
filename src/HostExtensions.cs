794219a889fb10b745e064bfb06e74eb447ec2f9
Nail Makhmutov <makhmutov.nail@gmail.com>
2019-07-30 21:42:31 +0300

    Updatge to dotnet core 3 preview

---
+++ src/HostExtensions.cs
@@ -0,0 +1,51 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Elwark.Extensions.AspNet
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDbContext<TContext>(this IHost host) where TContext : DbContext =>
+            MigrateDbContext<TContext>(host, (_, __) => { });
+
+        public static IHost MigrateDbContext<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder)
+            where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+                    InvokeSeeder(seeder, context, services);
+
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex,
+                        $"An error occurred while migrating the database used on context {typeof(TContext).Name}");
+                }
+            }
+
+            return host;
+        }
+        
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services)
+            where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
